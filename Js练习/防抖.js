e915d05c00dfdb745873dbcb1647e19af91f6a1d
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-27 02:03:07 +0800

    Create 防抖.js

---
+++ Js练习/防抖.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * 一个搜索输入框， 用户不停的进行输入（这个时候就是抖动的过程）， 等用户输入停止之后，再触发搜索
+ * 多次点击只触发一次
+ * 
+ * */
+
+function debounce (fn, wait) {
+    let timer = null;
+    return function (...args) {
+        if(timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            fn(...args);
+        }, wait);
+    }
+}

81bd64ce45168fb2be4a221f4cda3be06fdc0378
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-27 05:44:34 +0800

    Update LazyMan_Promise实现.js

--- Js练习/LazyMan_Promise实现.js
+++ Js练习/LazyMan_Promise实现.js
@@ -44,25 +44,26 @@ class LazyMan_Promise {
         this.tasks.push(() => {
             console.log('sayHi: ' + name);
         })
+        return this;
     }
 
     eat(name) {
         this.tasks.push(() => {
             console.log('eat: ' + name)
         })
+        return this;
     }
 
     sleep(timer) {
         this.tasks.push({sleep: timer})
+        return this;
     }
 
     sleepFirst(timer) {
         this.tasks.unshift({sleep: timer})
+        return this;
     }
 }
 
 let man = new LazyMan_Promise("w");
-man.eat('hi');
-man.sleep(1000);
-man.sleepFirst(2000);
-man.eat('hhh');
+man.eat('hi').sleep(1000).sleepFirst(2000).eat('hhh');

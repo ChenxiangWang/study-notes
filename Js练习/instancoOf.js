8d7e0c1e758c83d38f3b9c7f5dccb8927508ed92
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-27 18:42:31 +0800

    新增constructor判断方法

--- Js练习/instancoOf.js
+++ Js练习/instancoOf.js
@@ -1,13 +1,49 @@
-function instanceOf(left, fn) {
-    let check = left.__proto__;
-    let target = fn.prototype;
-    while (check) {
-        if (check === target) {
+class Person {
+
+}
+
+class Employee extends Person {
+
+}
+
+class Student extends Employee {
+
+}
+
+/**
+ *  constructor 判断
+ * */
+function instanceOf1 (type, obj) {
+    let p = obj.__proto__;
+    while (p) {
+        if (p.constructor === type) {
             return true;
         }
-        check = check.__proto__;
+        p = p.__proto__;
     }
     return false;
 }
 
-console.log(instanceOf(new Array([1]), Array));
+/**
+ *  原型判断
+ *
+ * */
+
+function instanceOf2 (type, obj) {
+    let target = type.prototype;
+    let p = obj.__proto__;
+    while (p) {
+        if (target === p) {
+            return true;
+        }
+        p = p.__proto__;
+    }
+    return  false;
+}
+
+let s = new Student();
+let p = new Person();
+
+console.log(instanceOf1(Person, s));
+
+console.log(instanceOf2(Person, s));

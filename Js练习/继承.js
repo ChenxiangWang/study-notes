10dd450dc24afd6970fe7a263951fdb20855c041
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-28 20:27:40 +0800

    Update 继承.js

--- Js练习/继承.js
+++ Js练习/继承.js
@@ -1,20 +1,30 @@
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
+/**
+ *  原型继承： 子类的原型 ---> 父类的实例
+ *  parent class: A
+ *  child class : B
+ *  B.prototype = new A()
+ * */
+function A () {}
+function B () {}
+B.prototype = new A();
+
+/**
+ *  构造函数内实现的继承: child class 内调用构造函数 D.call
+ *  子类的的原型还是子类的原型，但是子类的无法接触到父类原型的方法
+ * */
+function C() {}
+
+function D() {
+    D.call(this);
 }
 
-class Student extends Person {
-    constructor(name, score) {
-        super(name);
-        this.score = score;
-    }
+/**
+ * 组合继承：
+ *
+ * */
 
-    introducer() {
-        console.log(this.name + this.score);
-    }
+function E(){}
+F.proptype = new E();
+function F() {
+    E.call(this);
 }
-
-
-const p = new Student('chenxiang', 100);
-p.introducer();

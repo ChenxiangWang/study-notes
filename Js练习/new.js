63d9a503bde9bf3d9f5b8da2243cee3dbe36efac
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-28 20:27:42 +0800

    Update new.js

--- Js练习/new.js
+++ Js练习/new.js
@@ -1,10 +1,20 @@
-/**
- * 建立起引用关系即可
- *
- * */
-
-function new2 (Constructor, ...args) {
-    let obj = {};
-    obj.__proto__ = Constructor.prototype;
-    return obj;
+function A(name) {
+    this.name = name;
 }
+A.prototype = {
+    sayHi: function () {
+        console.log(this.name);
+    }
+}
+
+function new2(A, name) {
+    let obj = {}; // 1. 创建一个空白对象
+    obj.__proto__ = A.prototype; //2. 构建引用关系
+    A.call(obj, name); // 3. 执行函数 初始化对象
+    return obj; // 4. 返回对象
+}
+
+let obj = new2(A, 'wang');
+obj.sayHi();
+console.log(obj);
+
